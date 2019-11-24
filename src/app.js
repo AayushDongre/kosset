9aebbaeb48f9a11c4e83fbc1a814dafc2814fcba
aayush <aayushdongre4@gmail.com>
2019-11-24 17:31:54 +0530

    yess

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./routers/Router";
+import { Provider } from 'react-redux';
 import "normalize.css/normalize.css";
 import 'jquery'
 import 'popper.js'
@@ -10,7 +11,7 @@ import "./styles/styles.scss";
 
 
 const jsx = (
-         <Router />
+        <Router />
 );
 
 
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="yes">
+            yes
+        </div>
+    )
+}
+
+export default Hero;
