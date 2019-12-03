d86cec405257888f87ad874eefdc6e87be5d0e46
aayush <aayushdongre4@gmail.com>
2019-12-04 04:21:39 +0530

    banner 2

--- src/components/ProductBanner2.js
+++ src/components/ProductBanner2.js
@@ -4,13 +4,11 @@ import React from "react";
 const ProductBanner2 = () => {
     return (
         <div className="productBanner2 row">
-            <div className="col-md-6">
+            <div className="productBanner2-image col-md-6">
             </div>
-            <div className="col-md-6">
+            <div className="productBanner2-text col-md-6 ">
                 <ul>
-                    <li>yes</li>
-                    <li>yes</li>
-                    <li>yes</li>
+                    <li>Eco-friendly packets for easy disposal</li>
                 </ul>
             </div>
         </div>
