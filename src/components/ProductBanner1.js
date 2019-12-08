e9658ffc39f392b1fd8196bf107cbb565623ffa4
therexone <renerjake@gmail.com>
2019-12-08 18:44:49 +0530

    Styling Fixes

--- src/components/ProductBanner1.js
+++ src/components/ProductBanner1.js
@@ -4,14 +4,15 @@ import React from "react";
 const ProductBanner1 = () => {
     return (
         <div className="productBanner1 row">
-            <div className="productBanner1-text col-md-6 ">
-                <p className=" banner1-title">Kosset Sanitary Pads</p>
-                <p>are made of cotton and organic polymer, making them a better alternative for You and the Earth.
+            <div className="productBanner1-text col-md-6 px-lg-5">
+                <p className=" banner1-title px-lg-5 px-0 ">Kosset Sanitary Pads</p>
+                <p className="px-lg-5 px-0"> made of cotton and organic polymer, making them a better alternative for You and the Earth.
                 Get rid of chemically processed,
                 plastic containing sanitary pads and say
                 goodbye to rashes and irritation.</p>
             </div>
-            <div className="productBanner1-image col-md-6">
+            <div className="productBanner1-image px-0 col-lg-6">
+            <img src="/static/img/sec1.webp" className="img-fluid"/>
             </div>
         </div>
     )
--- src/components/ProductBanner2.js
+++ src/components/ProductBanner2.js
@@ -4,17 +4,18 @@ import React from "react";
 const ProductBanner2 = () => {
     return (
         <div className="productBanner2 row">
-            <div className="productBanner2-image col-md-6">
+            <div className="productBanner2-image px-0 col-md-6">
+                <img src="/static/img/sec2.webp" className="img-fluid" />
             </div>
             <div className="productBanner2-text col-md-6 ">
-                <ul>
-                    <li>Eco-friendly packets for easy disposal</li>
-                    <li>Customize your box in-sync with your flow</li>
-                    <li>Your period essentials deliverd to your doorstep</li>
-                </ul>
+                    <ul>
+                        <li>•  Eco-friendly packets for easy disposal</li>
+                        <li>•  Customize your box in-sync with your flow</li>
+                        <li>•  Your period essentials deliverd to your doorstep</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
-}
-
+            )
+        }
+        
 export default ProductBanner2;
